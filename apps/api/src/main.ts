4033061987f4cc3925ce5a6c3ea9f93f66acb6f5
linzheng <alidlg9045@126.com>
2024-12-31 11:31:42 +0800

    feat: signUp api in NestJS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
+  app.useGlobalPipes(new ValidationPipe({
+    // 将传入数据转换为 DTO 类型
+    transform: true,
+    // 删除不在 DTO 中的任何属性
+    whitelist: true,
+  }));
 }
 bootstrap();
+
