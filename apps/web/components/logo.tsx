3db96c55dcac630e730cac3226f5fea06d02dfc7
linzheng <alidlg9045@126.com>
2025-01-07 23:41:16 +0800

    feat: stores page

--- apps/web/components/logo.tsx
+++ apps/web/components/logo.tsx
@@ -4,8 +4,8 @@ import { PiLightningFill } from "react-icons/pi";
 export default function Logo() {
   return (
     <Link href={'/'} className="flex gap-1 items-center justify-center p-2 bg-purple-600 rounded-md">
-      <PiLightningFill size={24} />
-      <span className="font-semibold text-2xl">SHOP</span>
+      <PiLightningFill size={24} className="text-white" />
+      <span className="font-semibold text-2xl text-white">SHOP</span>
     </Link>
   )
 }
--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link"
 
 import UserButton from "@/features/auth/components/user-button"
+import StoreSign from "@/features/store/components/store-sign"
 
 import Logo from "./logo"
 import { ModeToggle } from "./mode-toggle"
 
 export default function Navbar() {
+
   return (
     <nav className="h-16 pt-4 px-6 flex items-center justify-between">
       <Logo />
-      <div className="flex items-center gap-2">
-        <Link href="/shops/create">Create Shop</Link>
+      <div className="flex items-center gap-4">
+        <StoreSign />
         <ModeToggle />
         <UserButton />
       </div>
