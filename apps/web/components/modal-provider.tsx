cbec2a0eb3e9299a24bf5306ea8a3f94829acedc
linzheng <alidlg9045@126.com>
2025-01-10 13:53:47 +0800

    feat: size CRUD in NextJS

--- apps/web/components/modal-provider.tsx
+++ apps/web/components/modal-provider.tsx
@@ -5,15 +5,19 @@ import EditBillboardModal from "@/features/billboard/components/edit-billboard-m
 import { useBillboardModal } from "@/features/billboard/store/use-billboard-modal";
 import CreateCategoryFormModal from "@/features/category/components/create-category-form-modal";
 import { useCategoryModal } from "@/features/category/store/use-category-modal";
+import CreateSizeFormModal from "@/features/size/components/create-size-form-modal";
+import { useSizeModal } from "@/features/size/store/use-size-modal";
 
 export default function ModalProvider() {
   const { isOpen: isOpenBillboard } = useBillboardModal()
   const { isOpen: isOpenCategory } = useCategoryModal()
+  const { isOpen: isOpenSize } = useSizeModal()
   return (
     <>
       {isOpenBillboard && <CreateBillboardFormModal />}
       <EditBillboardModal />
       {isOpenCategory && <CreateCategoryFormModal />}
+      {isOpenSize && <CreateSizeFormModal />}
     </>
   )
 }
